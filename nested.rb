089b3a56af1038a022b44bfd9d38fa8f483828a3
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-03 15:51:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,10 +81,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  
-  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-  programmer_hash  
+ 
+  programmer_hash[:yukihiro_matsumoto] = {}
 end
 
 def changing_alan
