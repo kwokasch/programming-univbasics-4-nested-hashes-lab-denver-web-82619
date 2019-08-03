4b8e3a1d127890e0053f5da8d44dec3a1d639ffc
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-03 16:17:30 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,9 +123,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  
-  dennis_ritchies_languages = [
-  "C"]
-  programmer_hash[:dennis_ritchie] [:languages] = dennis_ritchies_languages.push("Assembly")
+ 
+  programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
   
 end
