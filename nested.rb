d59eadf13fa63c57a95ae50d9950c24475a7cf7b
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-03 16:04:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,9 +103,10 @@ def changing_alan
     
     
      alans_new_info = "GUI"
+     
      programmer_hash[:alan_kay][:known_for] = alans_new_info
      
-end
+     programmer_hash
 
 def adding_to_dennis
 	programmer_hash = 
