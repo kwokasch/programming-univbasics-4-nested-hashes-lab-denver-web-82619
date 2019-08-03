b14021c3da95cb539c0ef0e34c711cd2503ab2c6
Katie Wokasch <katie.wokasch@gmail.com>
2019-08-03 15:59:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,7 @@ def adding_matz
 
 end
 
- 
-  
+
 def changing_alan
 	programmer_hash = 
  		{
@@ -99,9 +98,12 @@ def changing_alan
           :languages => ["C"]
         }
      }
+    
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+    
+    
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
      
 end
 
